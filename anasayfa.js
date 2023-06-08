c0b4b637bfe5445c187f4d4add9bd55eabdfd3a5
Ebru Küçük <kucukkebru@gmail.com>
2023-06-09 00:59:42 +0300

    güncelleme

--- anasayfa.js
+++ anasayfa.js
@@ -5,3 +5,18 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.right = "-200px";
 }
+window.onload = function() {
+    document.getElementById("myModal").style.display = "none";
+  };
+
+  function showModal() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.body.classList.add("modal-open");
+  }
+  
+  function hideModal() {
+    document.getElementById("myModal").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+    document.body.classList.remove("modal-open");
+  }
